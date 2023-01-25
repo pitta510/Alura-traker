5f301a28afa59f98cd9b1367f10c3c3400cf889f
Felipe Garcia <felipegarcia@MacBook-Air-de-Felipe.local>
2023-01-25 15:38:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral></BarraLateral>
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario @aoSalvarTarefa="salvarTarefa">
+
+      </Formulario>
+      <div class="lista">
+        <Tarefa v-for="(tarefa, index ) in tarefas" :key="index" :tarefa="tarefa"/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+import Tarefa from "./components/Tarefa.vue";
+import ITarefa from "@/interfaces/ITarefa"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    Formulario,
+    Tarefa,
+  },
+  data() {
+    return {
+      tarefas:[] as ITarefa[]
+    }
+  },
+  methods:{
+    salvarTarefa (tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    }
+  }
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">{{tarefa.descricao}}</div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos " />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import ITarefa from "@/interfaces/ITarefa";
+import { defineComponent, PropType } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "TarefaVue",
+  components: {
+    Cronometro,
+  },
+  props:{
+    tarefa:{
+        type: Object as PropType<ITarefa>,
+        required: true
+    }
+  }
+});
+</script>
+
+<style scoped>
+    .box {
+        background: #FAF0CA;
+    }
+</style>
