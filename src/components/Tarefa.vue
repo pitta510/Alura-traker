8d79d36f12a85a40bdd05f82c1055ef2399cbccb
pitta510 <pitta_510@hotmail.com>
2023-02-09 14:07:27 -0300

    second commit

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
-      <div class="column is-7">{{tarefa.descricao}}</div>
+      <div class="column is-7">{{tarefa.descricao || 'Tarefa sem descrição'}}</div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos " />
       </div>
     </div>
-  </div>
+  </Box>
 </template>
 
 <script lang="ts">
 import ITarefa from "@/interfaces/ITarefa";
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType } from "vue"; 
 import Cronometro from "./Cronometro.vue";
+import Box from "./Box.vue";
 
 export default defineComponent({
   name: "TarefaVue",
   components: {
     Cronometro,
+    Box
   },
   props:{
     tarefa:{
@@ -28,8 +30,3 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
-    .box {
-        background: #FAF0CA;
-    }
-</style>
